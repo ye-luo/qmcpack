af932207a6e6dbc817946b0df5b8853035b0c2e9
Ye Luo <yeluo@anl.gov>
2020-03-10 10:24:05 -0500

    Update license header.

--- src/QMCWaveFunctions/lcao/SoaLocalizedBasisSetRMG.h
+++ src/QMCWaveFunctions/lcao/SoaLocalizedBasisSetRMG.h
@@ -2,11 +2,11 @@
 // This file is distributed under the University of Illinois/NCSA Open Source License.
 // See LICENSE file in top directory for details.
 //
-// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+// Copyright (c) 2020 QMCPACK developers.
 //
-// File developed by:
+// File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
 //
-// File created by: Jeongnim Kim, jeongnim.kim@intel.com, Intel Corp.
+// File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
 
 
