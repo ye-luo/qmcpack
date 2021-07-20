743a44efd640d1cf361b5f5fc05ccfbd3c99bd0c
Miles <mnaletich@gmai.com>
2021-07-20 19:47:40 +0000

    Updated ompBLAS test.

--- src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
+++ src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
@@ -17,38 +17,47 @@
 #include <iostream>
 #include "OMPTarget/OMPallocator.hpp"
 #include "OMPTarget/ompBLAS.hpp"
+#include <OhmmsPETE/OhmmsVector.h>
+#include <OhmmsPETE/OhmmsMatrix.h>
+#include <Numerics/OhmmsBlas.h>
+
+/*
+ompBLAS_status gemv(ompBLAS_handle&    handle,
+                    const char         trans,
+                    const int          m,
+                    const int          n,
+                    const float        alpha,
+                    const float* const A,
+                    const int          lda,
+                    const float* const x,
+                    const int          incx,
+                    const float        beta,
+                    float* const       y,
+                    const int          incy);
+*/
 
 namespace qmcplusplus
 {
 TEST_CASE("OmpBLAS gemv", "[OMP]")
 {
-  typedef std::vector<double, OMPallocator<double>> vec_t;
-  vec_t A(3);
 
-  // iterator
-  vec_t::iterator ia = A.begin();
-  for (; ia != A.end(); ia++)
-  {
-    *ia = 3.1;
-  }
+  using vec_t = Vector<double, OMPallocator<double>>;
+  using mat_t = Matrix<double, OMPallocator<double>>;
 
-  auto* A_ptr = A.data();
-  PRAGMA_OFFLOAD("omp target teams distribute map(always, tofrom:A_ptr[0:2])")
-  for (int i = 0; i < 2; i++)
-  {
-    A_ptr[i] += i;
-  }
+ PRAGMA_OFFLOAD("omp target teams distribute map(to : B[:N * N], A[:N]) map(from : C[:N], D[:N])")
+   for(int i = 0; i < N; i++)
+    {
+      vec_t A(i); // Input vector
+      mat_t B(i); // Input matrix
+      vec_t C(i); // Result vector (test)
+      vec_t D(i) // Result vector BLAS
+      
+      gemv(dummy, "T", i, i, 1.0, B, 1, A, 1, 1.0, C, 1); // tests omp gemv
+      BLAS.gemv(i, i, B, A, D); // BLAS gemv
+      REQUIRE(std::equal(C, D) == true);  // Compares results and determines if each loop passes
+    }
 
-  REQUIRE(A[0] == Approx(3.1));
-  REQUIRE(A[1] == Approx(4.1));
-
-  A.resize(256);
-  A_ptr = A.data();
-  PRAGMA_OFFLOAD("omp target teams distribute map(always, tofrom:A_ptr[0:2])")
-  for (int i = 0; i < 2; i++)
-  {
-    A_ptr[i] += i;
-  }
+  
 }
 
 } // namespace qmcplusplus
