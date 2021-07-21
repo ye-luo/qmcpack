d5efd4f8e594e0843b882c184ae39dd78dde5b0f
Miles <mnaletich@gmai.com>
2021-07-21 22:38:41 +0000

    Compiled gemv() test, fails with segmentation violation.

--- src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
+++ src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
@@ -9,7 +9,6 @@
 // File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
 //////////////////////////////////////////////////////////////////////////////////////
 
-
 #include "catch.hpp"
 
 #include <memory>
@@ -19,50 +18,41 @@
 #include "OMPTarget/ompBLAS.hpp"
 #include <OhmmsPETE/OhmmsVector.h>
 #include <OhmmsPETE/OhmmsMatrix.h>
-<<<<<<< HEAD
-#include <Numerics/OhmmsBlas.h>
-=======
 #include <CPU/BLAS.hpp>
 #include <checkMatrix.hpp>
->>>>>>> 2c557eedd27418398990c8c9102bd71433e76627
-
-/*
-ompBLAS_status gemv(ompBLAS_handle&    handle,
-                    const char         trans,
-                    const int          m,
-                    const int          n,
-                    const float        alpha,
-                    const float* const A,
-                    const int          lda,
-                    const float* const x,
-                    const int          incx,
-                    const float        beta,
-                    float* const       y,
-                    const int          incy);
-*/
 
 namespace qmcplusplus
 {
 TEST_CASE("OmpBLAS gemv", "[OMP]")
 {
-
+  int N = 100;
   using vec_t = Vector<double, OMPallocator<double>>;
   using mat_t = Matrix<double, OMPallocator<double>>;
 
- PRAGMA_OFFLOAD("omp target teams distribute map(to : B[:N * N], A[:N]) map(from : C[:N], D[:N])")
-   for(int i = 0; i < N; i++)
-    {
-      vec_t A(i); // Input vector
-      mat_t B(i); // Input matrix
-      vec_t C(i); // Result vector (test)
-      vec_t D(i) // Result vector BLAS
-      
-      gemv(dummy, "T", i, i, 1.0, B, 1, A, 1, 1.0, C, 1); // tests omp gemv
-      BLAS.gemv(i, i, B, A, D); // BLAS gemv
-      REQUIRE(std::equal(C, D) == true);  // Compares results and determines if each loop passes
-    }
-
+  ompBLAS::ompBLAS_handle handle;
   
+  for(int i = 0; i < N; i++)
+    {
+      handle = i;
+      vec_t A(i); // Input vector
+      mat_t B(i, i); // Input matrix
+      vec_t C(i); // Result vector (test)
+      vec_t D(i); // Result vector BLAS
+      
+      ompBLAS:: gemv(handle, 'T', i, i, 1.0, B.data(), 1, A.data(), 1, 1.0, C.data(), 1); // tests omp gemv
+      BLAS:: gemv(i, i, B.data(), A.data(), D.data());
+
+      bool are_same = true;
+      int index = 0;
+      do
+	{
+	  are_same = C.operator[](i) == D.operator[](i);
+	  index++;
+	} while(are_same == true && index < N);
+
+      REQUIRE(are_same == true);
+      
+    }
 }
 
 } // namespace qmcplusplus
