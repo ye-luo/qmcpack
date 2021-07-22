bb728636294956f23a7dfa79f32bf1836e156857
Ye Luo <yeluo@anl.gov>
2021-07-22 16:49:52 -0500

    Working!!!!

--- src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
+++ src/Platforms/tests/OMPTarget/test_ompBLAS.cpp
@@ -25,34 +25,47 @@ namespace qmcplusplus
 {
 TEST_CASE("OmpBLAS gemv", "[OMP]")
 {
-  int N = 100;
+  const int N = 100;
   using vec_t = Vector<double, OMPallocator<double>>;
   using mat_t = Matrix<double, OMPallocator<double>>;
 
   ompBLAS::ompBLAS_handle handle;
-  
-  for(int i = 0; i < N; i++)
+
+  vec_t A(N);    // Input vector
+  mat_t B(N, N); // Input matrix
+  vec_t C(N);    // Result vector (test)
+  vec_t D(N);    // Result vector BLAS
+
+  for (int dim = 1; dim <= N; dim++)
+  {
+    handle = dim;
+    for (int i = 0; i < dim; i++)
     {
-      handle = i;
-      vec_t A(i); // Input vector
-      mat_t B(i, i); // Input matrix
-      vec_t C(i); // Result vector (test)
-      vec_t D(i); // Result vector BLAS
-      
-      ompBLAS:: gemv(handle, 'T', i, i, 1.0, B.data(), 1, A.data(), 1, 1.0, C.data(), 1); // tests omp gemv
-      BLAS:: gemv(i, i, B.data(), A.data(), D.data());
-
-      bool are_same = true;
-      int index = 0;
-      do
-	{
-	  are_same = C.operator[](i) == D.operator[](i);
-	  index++;
-	} while(are_same == true && index < N);
-
-      REQUIRE(are_same == true);
-      
+      A[i] = i;
+      for (int j = 0; j < dim; j++)
+        B.data()[i * dim + j] = i + j;
     }
+
+    A.updateTo();
+    B.updateTo();
+
+    ompBLAS::gemv(handle, 'T', dim, dim, 1.0, B.device_data(), dim, A.device_data(), 1, 0.0, C.device_data(),
+                  1); // tests omp gemv
+    BLAS::gemv(dim, dim, B.data(), A.data(), D.data());
+
+    C.updateFrom();
+
+    //std::cout << "checking dim " << dim << std::endl;
+
+    bool are_same = true;
+    int index     = 0;
+    do
+    {
+      are_same = C[index] == D[index];
+      CHECK(C[index] == D[index]);
+      index++;
+    } while (are_same == true && index < dim);
+  }
 }
 
 } // namespace qmcplusplus
